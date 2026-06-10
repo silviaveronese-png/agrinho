d6907056030f511ac17135e1fc2f89c708ed4802
silviaveronese-png <silvia.veronese@escola.pr.gov.br>
2026-06-10 14:39:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const mensagens = [
+    "🌱 Cada planta cultivada ajuda a construir um futuro mais verde.",
+    "💚 Sustentabilidade começa com pequenas ações diárias.",
+    "🌎 Uma horta sustentável contribui para um planeta melhor.",
+    "🍅 Produzir seu próprio alimento é cuidar da saúde e do meio ambiente.",
+    "♻️ Reutilizar materiais reduz o desperdício e protege a natureza."
+];
 
+function mostrarMensagem() {
+    const indice = Math.floor(Math.random() * mensagens.length);
+
+    document.getElementById("mensagem").textContent =
+        mensagens[indice];
+}
